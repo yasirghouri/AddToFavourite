54669d9f781843b12765716b1966e3eb3569ed3a
yasirghouri <yasir.ag7@gmail.com>
2021-01-13 12:27:00 +0500

    adding Context-API functionality

--- src/store/reducers/products.js
+++ src/store/reducers/products.js
@@ -1,49 +1,49 @@
-import { TOGGLE_FAV } from '../actions/products';
+import { TOGGLE_FAV } from "../actions/products";
 
 const initialState = {
   products: [
     {
-      id: 'p1',
-      title: 'Red Scarf',
-      description: 'A pretty red scarf.',
-      isFavorite: false
+      id: "p1",
+      title: "Red Scarf",
+      description: "A pretty red scarf.",
+      isFavorite: false,
     },
     {
-      id: 'p2',
-      title: 'Blue T-Shirt',
-      description: 'A pretty blue t-shirt.',
-      isFavorite: false
+      id: "p2",
+      title: "Blue T-Shirt",
+      description: "A pretty blue t-shirt.",
+      isFavorite: false,
     },
     {
-      id: 'p3',
-      title: 'Green Trousers',
-      description: 'A pair of lightly green trousers.',
-      isFavorite: false
+      id: "p3",
+      title: "Green Trousers",
+      description: "A pair of lightly green trousers.",
+      isFavorite: false,
     },
     {
-      id: 'p4',
-      title: 'Orange Hat',
-      description: 'Street style! An orange hat.',
-      isFavorite: false
-    }
-  ]
+      id: "p4",
+      title: "Orange Hat",
+      description: "Street style! An orange hat.",
+      isFavorite: false,
+    },
+  ],
 };
 
 const productReducer = (state = initialState, action) => {
   switch (action.type) {
     case TOGGLE_FAV:
       const prodIndex = state.products.findIndex(
-        p => p.id === action.productId
+        (p) => p.id === action.productId
       );
       const newFavStatus = !state.products[prodIndex].isFavorite;
       const updatedProducts = [...state.products];
       updatedProducts[prodIndex] = {
         ...state.products[prodIndex],
-        isFavorite: newFavStatus
+        isFavorite: newFavStatus,
       };
       return {
         ...state,
-        products: updatedProducts
+        products: updatedProducts,
       };
     default:
       return state;
