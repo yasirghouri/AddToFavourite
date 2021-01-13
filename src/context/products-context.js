0689b6edcf87ab6327fadc7b6f695592bea8dccb
yasirghouri <yasir.ag7@gmail.com>
2021-01-13 15:45:40 +0500

    adding context/redux alternative 'custom hook'

--- src/context/products-context.js
+++ src/context/products-context.js
@@ -1,46 +1,46 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
 export const ProductsContext = React.createContext({
   products: [],
-  toggleFav: (id) => {},
+  toggleFav: (id) => {}
 });
 
-export default (props) => {
+export default props => {
   const [productsList, setProductsList] = useState([
     {
-      id: "p1",
-      title: "Red Scarf",
-      description: "A pretty red scarf.",
-      isFavorite: false,
+      id: 'p1',
+      title: 'Red Scarf',
+      description: 'A pretty red scarf.',
+      isFavorite: false
     },
     {
-      id: "p2",
-      title: "Blue T-Shirt",
-      description: "A pretty blue t-shirt.",
-      isFavorite: false,
+      id: 'p2',
+      title: 'Blue T-Shirt',
+      description: 'A pretty blue t-shirt.',
+      isFavorite: false
     },
     {
-      id: "p3",
-      title: "Green Trousers",
-      description: "A pair of lightly green trousers.",
-      isFavorite: false,
+      id: 'p3',
+      title: 'Green Trousers',
+      description: 'A pair of lightly green trousers.',
+      isFavorite: false
     },
     {
-      id: "p4",
-      title: "Orange Hat",
-      description: "Street style! An orange hat.",
-      isFavorite: false,
-    },
+      id: 'p4',
+      title: 'Orange Hat',
+      description: 'Street style! An orange hat.',
+      isFavorite: false
+    }
   ]);
 
-  const toggleFavorite = (productId) => {
-    setProductsList((currentProdList) => {
-      const prodIndex = currentProdList.findIndex((p) => p.id === productId);
+  const toggleFavorite = productId => {
+    setProductsList(currentProdList => {
+      const prodIndex = currentProdList.findIndex(p => p.id === productId);
       const newFavStatus = !currentProdList[prodIndex].isFavorite;
       const updatedProducts = [...currentProdList];
       updatedProducts[prodIndex] = {
         ...currentProdList[prodIndex],
-        isFavorite: newFavStatus,
+        isFavorite: newFavStatus
       };
       return updatedProducts;
     });
